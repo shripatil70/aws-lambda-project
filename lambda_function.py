b85a2431db09d9eea55fe35bd36095fc285d10be
Dhanashri Patil <patil.dhanashrik@gmail.com>
2026-04-14 11:31:50 +0530

    Add lambda_handler function to return HTML response

--- lambda_function.py
+++ lambda_function.py
@@ -1,1 +1,6 @@
-##
+def lambda_handler(event, context):
+    return {
+        'statusCode': 200,
+        'headers': {'Content-Type': 'text/html'},
+        'body': "<h1>Hello from Lambda (AWS) and Dhanashri</h1>"
+    }
